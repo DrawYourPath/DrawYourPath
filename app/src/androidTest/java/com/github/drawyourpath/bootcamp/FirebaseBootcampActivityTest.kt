f35254b1b14884be8b5405fac2111419e6a8c903
HugoHof <hugo.hof@epfl.ch>
2023-03-01 18:51:59 +0100

    correct test of the firebase for the automatic test

--- app/src/androidTest/java/com/github/drawyourpath/bootcamp/FirebaseBootcampActivityTest.kt
+++ app/src/androidTest/java/com/github/drawyourpath/bootcamp/FirebaseBootcampActivityTest.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.test.core.app.ActivityScenario
 import androidx.test.core.app.ActivityScenario.launch
 import androidx.test.core.app.ApplicationProvider.getApplicationContext
+import androidx.test.espresso.Espresso.closeSoftKeyboard
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.assertion.ViewAssertions.matches
@@ -23,17 +24,24 @@ class FirebaseBootcampActivityTest {
         var intent = Intent(getApplicationContext(), FirebaseBootcampActivity::class.java)
         intent.putExtra("isRunningTestForDataBase", true)
         var t: ActivityScenario<FirebaseBootcampActivity> = launch(intent)
+        closeSoftKeyboard()
         onView(withId(R.id.textMailFirebase)).perform(ViewActions.typeText("hugo.hof@epfl.ch"))
+        closeSoftKeyboard()
         onView(withId(R.id.textPhoneFirebase)).perform(ViewActions.typeText("12345678"))
+        closeSoftKeyboard()
         onView(withId(R.id.setButtonFirebase)).perform(ViewActions.click())
         onView(withId(R.id.textMailFirebase)).perform(ViewActions.clearText())
         onView(withId(R.id.textMailFirebase)).perform(ViewActions.typeText("eric.kurmann@epfl.ch"))
+        closeSoftKeyboard()
         onView(withId(R.id.textPhoneFirebase)).perform(ViewActions.clearText())
         onView(withId(R.id.textPhoneFirebase)).perform(ViewActions.typeText("987654321"))
+        closeSoftKeyboard()
         onView(withId(R.id.setButtonFirebase)).perform(ViewActions.click())
         onView(withId(R.id.textMailFirebase)).perform(ViewActions.clearText())
+        closeSoftKeyboard()
         onView(withId(R.id.textMailFirebase)).perform(ViewActions.typeText("hugo.hof@epfl.ch"))
         onView(withId(R.id.getButtonFirebase)).perform(ViewActions.click())
+        closeSoftKeyboard()
         onView(withId(R.id.textPhoneFirebase)).check(matches(withText("12345678")))
         t.close()
     }
