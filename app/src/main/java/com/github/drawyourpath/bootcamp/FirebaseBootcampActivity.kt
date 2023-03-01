123dd561a5fc64314cd90c54937366d99f040df5
HugoHof <hugo.hof@epfl.ch>
2023-03-01 18:21:18 +0100

    add tests in firebase bootcamp

--- app/src/main/java/com/github/drawyourpath/bootcamp/FirebaseBootcampActivity.kt
+++ app/src/main/java/com/github/drawyourpath/bootcamp/FirebaseBootcampActivity.kt
@@ -1,51 +1,32 @@
 package com.github.drawyourpath.bootcamp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.text.Editable
-import android.view.View
 import android.widget.Button
 import android.widget.EditText
-import android.widget.TextView
-import com.google.firebase.database.DatabaseReference
-import com.google.firebase.database.ktx.database
-import com.google.firebase.ktx.Firebase
-import java.util.concurrent.CompletableFuture
+import androidx.appcompat.app.AppCompatActivity
+
 
 class FirebaseBootcampActivity : AppCompatActivity() {
-    val db: DatabaseReference = Firebase.database.reference
 
+    var dataBase: Database = FireDatabase()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_firebase_bootcamp)
 
+        //test if we are running tests in the dataBase
+        val isTest: Boolean = intent.extras?.getBoolean("isRunningTestForDataBase") ?: false
+        if (isTest) {
+            dataBase = MockDataBase()
+        }
+
         val mailInput: EditText = findViewById(R.id.textMailFirebase)
         val phoneInput: EditText = findViewById(R.id.textPhoneFirebase)
 
-        val setButton : Button = findViewById(R.id.setButtonFirebase)
-        setButton.setOnClickListener{set(mailInput, phoneInput)}
+        val setButton: Button = findViewById(R.id.setButtonFirebase)
+        setButton.setOnClickListener { dataBase.set(mailInput, phoneInput) }
 
-        val getButton : Button = findViewById(R.id.getButtonFirebase)
-        getButton.setOnClickListener{get(mailInput, phoneInput)}
+        val getButton: Button = findViewById(R.id.getButtonFirebase)
+        getButton.setOnClickListener { dataBase.get(mailInput, phoneInput) }
     }
 
-    private fun set(email: EditText, phoneNumber: EditText) {
-
-        db.child(phoneNumber.text.toString()).setValue(email.text.toString())
-    }
-
-    private fun get(email: EditText, phoneNumber: EditText) {
-        val future = CompletableFuture<String>()
-
-        db.child(phoneNumber.text.toString()).get().addOnSuccessListener {
-            if (it.value == null) future.completeExceptionally(NoSuchFieldException())
-            else future.complete(it.value as String)
-        }.addOnFailureListener {
-            future.completeExceptionally(it)
-        }
-
-        future.thenAccept {
-            email.setText(it)
-        }
-    }
 }
---
+++ app/src/main/java/com/github/drawyourpath/bootcamp/MockDataBase.kt
@@ -0,0 +1,15 @@
+package com.github.drawyourpath.bootcamp
+
+import android.widget.EditText
+
+class MockDataBase : Database() {
+    private var dataMap: HashMap<String, String> = HashMap()
+    override fun get(email: EditText, phoneNumber: EditText) {
+        val number: String? = dataMap.get(email.text.toString())
+        phoneNumber.setText(number)
+    }
+
+    override fun set(email: EditText, phoneNumber: EditText) {
+        dataMap.put(email.text.toString(), phoneNumber.text.toString())
+    }
+}
