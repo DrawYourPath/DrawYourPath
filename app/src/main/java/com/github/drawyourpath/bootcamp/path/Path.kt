577ec0fe2f486b3ae57ee2ab365fecd7452a6aa3
Cristian Safta <saftakrist@gmail.com>
2023-03-09 23:03:58 +0100

    fixed an error in a test

--- app/src/main/java/com/github/drawyourpath/bootcamp/path/Path.kt
+++ app/src/main/java/com/github/drawyourpath/bootcamp/path/Path.kt
@@ -1,4 +1,89 @@
 package com.github.drawyourpath.bootcamp.path
+import com.google.type.Color
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.model.Polyline
+import com.google.android.gms.maps.model.PolylineOptions
 
+// Define a Path class that represents a runner's path.
 class Path {
+
+    // Constructor that creates an empty path.
+    constructor()
+
+    // Constructor that creates a path from a list of points.
+    constructor(points: List<LatLng>) {
+        this.points.addAll(points)
+    }
+
+    //private mutable list to store the path as a collection of points.
+    private val points = mutableListOf<LatLng>()
+
+    // Add a point to the path's list of points(at the end of the list).
+    fun addPoint(point: LatLng) {
+        points.add(point)
+    }
+
+    // Return an immutable list of the path's points.
+    fun getPoints(): List<LatLng> {
+        return points.toList()
+    }
+
+    // Clear the path's list of points.
+    fun clear() {
+        points.clear()
+    }
+
+    // Return the number of points in the path.
+    fun size(): Int {
+        return points.size
+    }
+
+    //helper function to calculate distance between two points(in meters)
+    private fun distance(point1: LatLng, point2: LatLng): Double {
+        val lat1 = point1.latitude
+        val lat2 = point2.latitude
+        val lon1 = point1.longitude
+        val lon2 = point2.longitude
+        val R = 6371e3 // metres
+        val φ1 = lat1 * Math.PI / 180 // φ, λ in radians
+        val φ2 = lat2 * Math.PI / 180
+        val Δφ = (lat2 - lat1) * Math.PI / 180
+        val Δλ = (lon2 - lon1) * Math.PI / 180
+        val a = Math.sin(Δφ / 2) * Math.sin(Δφ / 2) +
+                Math.cos(φ1) * Math.cos(φ2) *
+                Math.sin(Δλ / 2) * Math.sin(Δλ / 2)
+        val c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a))
+        return R * c
+
+    }
+
+    //function that returns the distance of the path in meters.
+    fun getDistance(): Double {
+        var distance = 0.0
+        for (i in 0 until points.size - 1) {
+            distance += distance(points[i], points[i + 1])
+        }
+        return distance
+    }
+
+    // Return a Polyline object representing the path.
+    fun getPolyline(): PolylineOptions {
+
+        // Create a new PolylineOptions object to define the appearance of the polyline.
+        val polylineOptions = PolylineOptions()
+        polylineOptions.color(Color.BLUE_FIELD_NUMBER)
+        polylineOptions.width(10f)
+
+
+        // Loop through the list of points and add them to the PolylineOptions object.
+        for (point in points) {
+            polylineOptions.add(point)
+        }
+
+        // Add the polyline to the map and return it.
+        return polylineOptions
+    }
+
+
 }
--- app/src/test/java/com/github/drawyourpath/bootcamp/path/PathTest.kt
+++ app/src/test/java/com/github/drawyourpath/bootcamp/path/PathTest.kt
@@ -1,4 +1,92 @@
 package com.github.drawyourpath.bootcamp.path
 
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.PolylineOptions
+import junit.framework.TestCase.assertEquals
+import junit.framework.TestCase.assertTrue
+import org.hamcrest.MatcherAssert.assertThat
+import org.junit.Test
+
 class PathTest {
+
+    @Test
+    fun `test creating empty path`() {
+        val path = Path()
+        assertEquals(0, path.size())
+    }
+
+    @Test
+    fun `test creating path from list of points`() {
+        val point1 = LatLng(1.0, 2.0)
+        val point2 = LatLng(3.0, 4.0)
+        val points = listOf(point1, point2)
+
+        val path = Path(points)
+
+        assertEquals(2, path.size())
+        assertTrue(path.getPoints().containsAll(points))
+    }
+
+    @Test
+    fun `addPoint adds a point to the path`() {
+        val path = Path()
+        path.addPoint(LatLng(1.0, 2.0))
+        assertEquals(1, path.size())
+    }
+
+    @Test
+    fun `getPoints returns an empty list for a new empty path`() {
+        val path = Path()
+        assertTrue(path.getPoints().isEmpty())
+    }
+
+    @Test
+    fun `getPoints returns a list of all added points`() {
+        val path = Path()
+        val point1 = LatLng(1.0, 2.0)
+        val point2 = LatLng(3.0, 4.0)
+        path.addPoint(point1)
+        path.addPoint(point2)
+        assertEquals(listOf(point1, point2), path.getPoints())
+    }
+
+    @Test
+    fun `clear removes all points from the path`() {
+        val path = Path()
+        path.addPoint(LatLng(1.0, 2.0))
+        path.clear()
+        assertTrue(path.getPoints().isEmpty())
+    }
+
+    @Test
+    fun `size returns the number of points in a non empty path`() {
+        val path = Path()
+        path.addPoint(LatLng(1.0, 2.0))
+        path.addPoint(LatLng(3.0, 4.0))
+        assertEquals(2, path.size())
+    }
+
+
+    @Test
+    fun `getDistance returns the distance between added points`() {
+        val path = Path()
+        path.addPoint(LatLng(0.0, 0.0))
+        path.addPoint(LatLng(0.0, 1.0))
+        assertEquals(111319.9, path.getDistance(), 500.0)
+
+    }
+
+
+    @Test
+    fun `getPolyline returns a PolylineOptions object`() {
+        val path = Path()
+        val point1 = LatLng(1.0, 2.0)
+        val point2 = LatLng(3.0, 4.0)
+        path.addPoint(point1)
+        path.addPoint(point2)
+        val polyline = path.getPolyline()
+        assertTrue(polyline is PolylineOptions)
+        assertEquals(listOf(point1, point2), polyline.points)
+    }
+
 }
