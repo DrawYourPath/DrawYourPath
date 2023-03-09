1ddedd9ed71d87cbee749a161b84aa895f9fc3de
Clément <clement.charmillot@epfl.ch>
2023-03-09 21:40:07 +0100

    fix: remove unused imports

--- app/src/main/java/com/github/drawyourpath/bootcamp/login/LoginActions.kt
+++ app/src/main/java/com/github/drawyourpath/bootcamp/login/LoginActions.kt
@@ -3,9 +3,7 @@ package com.github.drawyourpath.bootcamp.login
 import android.os.Bundle
 import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import android.widget.Button
 import com.github.drawyourpath.bootcamp.R
 
