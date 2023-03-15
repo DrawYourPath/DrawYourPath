585b54bf7b26f491df2aae89d92566a879c79dc6
damiengenoud <damien.genoud@epfl.ch>
2023-03-16 00:39:25 +0100

    Add a way to disconnect the user
    But it reconnects automatically.

--- app/src/main/java/com/epfl/drawyourpath/preferences/PreferencesFragment.kt
+++ app/src/main/java/com/epfl/drawyourpath/preferences/PreferencesFragment.kt
@@ -1,12 +1,32 @@
 package com.epfl.drawyourpath.preferences
 
+import android.content.Intent
 import android.os.Bundle
+import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
 import com.epfl.drawyourpath.R
+import com.epfl.drawyourpath.authentication.FirebaseAuth
+import com.epfl.drawyourpath.login.LoginActivity
 
 class PreferencesFragment : PreferenceFragmentCompat() {
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.preferences, rootKey)
+
+        setupDisconnect()
     }
-    //TODO: handle preferences
+
+    private fun setupDisconnect() {
+        // Adds the listener to log out and return to the login screen
+        findPreference<Preference>("disconnect")?.setOnPreferenceClickListener {
+            val auth = FirebaseAuth()
+            auth.signOut()
+            this.startActivity(Intent(this.context, LoginActivity::class.java))
+            true
+        }
+    }
+    //TODO: handle preference
 }
+
+
+
+
