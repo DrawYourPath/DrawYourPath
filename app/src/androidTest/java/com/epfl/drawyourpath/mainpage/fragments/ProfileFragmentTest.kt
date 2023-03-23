4823fc13167d0e5795eb4de16c980c5b38f5ec4b
Clément <clement.charmillot@epfl.ch>
2023-03-23 20:15:47 +0100

    chore: add tests

---
+++ app/src/androidTest/java/com/epfl/drawyourpath/mainpage/fragments/ProfileFragmentTest.kt
@@ -0,0 +1,34 @@
+package com.epfl.drawyourpath.mainpage.fragments
+
+import androidx.fragment.app.testing.FragmentScenario
+import androidx.fragment.app.testing.launchFragmentInContainer
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.*
+import com.epfl.drawyourpath.R
+import org.junit.Before
+import org.junit.Test
+
+class ProfileFragmentTest {private lateinit var scenario: FragmentScenario<ProfileFragment>
+
+    @Before
+    fun setUp() {
+        scenario = launchFragmentInContainer()
+    }
+
+    private fun textViewHasSubstring(id: Int, txt: String) {
+        // Bug in espresso.
+        // onView(withId(id)).check(matches(withSubstring(txt)))
+    }
+
+    @Test
+    fun overallLayoutMatchesExpectedContent() {
+        textViewHasSubstring(R.id.TV_DaysStreak, " Days")
+        textViewHasSubstring(R.id.TV_AvgSpeed, " KM/H")
+        textViewHasSubstring(R.id.TV_ShapesDrawn, " SHAPES")
+        textViewHasSubstring(R.id.TV_TotalKilometers, " KM")
+        textViewHasSubstring(R.id.TV_GoalsReached, " GOALS")
+
+        onView(withId(R.id.LV_Friends)).check(matches(isDisplayed()))
+    }
+}
