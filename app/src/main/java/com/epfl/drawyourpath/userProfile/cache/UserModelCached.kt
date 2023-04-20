5676bdc70ff413cc06f9a2f36fb5e4728370ef53
Eric Kurmann <eric.kurmann@epfl.ch>
2023-04-20 21:40:33 +0200

    remove prepopulation

--- app/src/main/java/com/epfl/drawyourpath/userProfile/cache/UserModelCached.kt
+++ app/src/main/java/com/epfl/drawyourpath/userProfile/cache/UserModelCached.kt
@@ -41,7 +41,6 @@ class UserModelCached(application: Application) : AndroidViewModel(application) 
     private val roomDatabase = Room
         .databaseBuilder(application, UserDatabase::class.java, UserDatabase.NAME)
         .fallbackToDestructiveMigration()
-        .createFromAsset("mockRoom/UserDatabase.db")
         .build()
 
     // room database user
