74c06d7d93bcc7839380c842ba053ffbda2243b7
Eric Kurmann <eric.kurmann@epfl.ch>
2023-04-24 15:19:33 +0200

    fix foreign key in points entity

--- app/src/main/java/com/epfl/drawyourpath/path/cache/PointsEntity.kt
+++ app/src/main/java/com/epfl/drawyourpath/path/cache/PointsEntity.kt
@@ -3,14 +3,11 @@ package com.epfl.drawyourpath.path.cache
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.ForeignKey
-import com.epfl.drawyourpath.userProfile.cache.UserEntity
 
 @Entity(
     tableName = "Points",
     primaryKeys = ["user_id", "run_id", "index"],
-    foreignKeys = [ForeignKey(
-        entity = UserEntity::class, parentColumns = ["id"], childColumns = ["user_id"], onDelete = ForeignKey.CASCADE
-    ), ForeignKey(entity = RunEntity::class, parentColumns = ["start_time"], childColumns = ["run_id"], onDelete = ForeignKey.CASCADE)],
+    foreignKeys = [ForeignKey(entity = RunEntity::class, parentColumns = ["user_id", "start_time"], childColumns = ["user_id", "run_id"], onDelete = ForeignKey.CASCADE)],
 )
 data class PointsEntity(
     @ColumnInfo("user_id")
