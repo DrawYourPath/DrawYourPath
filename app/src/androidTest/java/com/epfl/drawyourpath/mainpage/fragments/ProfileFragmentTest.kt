29835e84595c2eef68dc0a1c73f6cde9c459e737
Clément <clement.charmillot@epfl.ch>
2023-05-02 22:54:07 +0200

    fix: format

--- app/src/androidTest/java/com/epfl/drawyourpath/mainpage/fragments/ProfileFragmentTest.kt
+++ app/src/androidTest/java/com/epfl/drawyourpath/mainpage/fragments/ProfileFragmentTest.kt
@@ -8,7 +8,6 @@ import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
-import androidx.test.espresso.matcher.ViewMatchers.withSubstring
 import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.epfl.drawyourpath.R
