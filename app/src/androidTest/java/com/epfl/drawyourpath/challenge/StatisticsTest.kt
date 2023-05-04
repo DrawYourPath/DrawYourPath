4878aabd37e5068dc7f28df10a14a4940e9ab9c1
Clément <clement.charmillot@epfl.ch>
2023-05-04 19:11:51 +0200

    fix: add missing import

--- app/src/androidTest/java/com/epfl/drawyourpath/challenge/StatisticsTest.kt
+++ app/src/androidTest/java/com/epfl/drawyourpath/challenge/StatisticsTest.kt
@@ -1,9 +1,11 @@
 package com.epfl.drawyourpath.challenge
 
+import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.epfl.drawyourpath.userProfile.dailygoal.DailyGoal
 import org.hamcrest.MatcherAssert.assertThat
 import org.hamcrest.Matchers.`is`
 import org.junit.Test
+import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
 class StatisticsTest {
