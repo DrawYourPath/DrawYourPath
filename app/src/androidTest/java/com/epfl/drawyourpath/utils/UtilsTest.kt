7367f899c0a40207b02978af0e8d78ea49ead0a8
Clément <100228798+clementcharmillot@users.noreply.github.com>
2023-05-09 17:38:26 +0200

    169 coordinates to ink (#181)
    * feat: add conversion functions
    
    * action: auto-formatting
    
    * fix: add tests
    
    * fix: rename functions to match API names
    
    ---------
    
    Co-authored-by: DrawYourPathBot <bot@drawyourpath.ch>

---
+++ app/src/androidTest/java/com/epfl/drawyourpath/utils/UtilsTest.kt
@@ -0,0 +1,43 @@
+package com.epfl.drawyourpath.utils
+
+import com.google.android.gms.maps.model.LatLng
+import org.hamcrest.MatcherAssert.assertThat
+import org.hamcrest.Matchers.`is`
+import org.junit.Assert.assertTrue
+import org.junit.Test
+
+class UtilsTest {
+
+    @Test
+    fun coordinateToInkReturnsExpectedValues() {
+        val coordinate = LatLng(1.0, 1.0)
+
+        val point = Utils.coordinateToPoint(coordinate)
+
+        assertTrue(point.x < Math.PI)
+        assertTrue(point.x > 0.0)
+
+        assertTrue(point.y < Math.PI)
+        assertTrue(point.y > 0.0)
+    }
+
+    @Test
+    fun convertEmptyListOfCoordinatesReturnsEmptyStroke() {
+        val stroke = Utils.coordinatesToStroke(emptyList())
+
+        assertTrue(stroke.points.isEmpty())
+    }
+
+    @Test
+    fun pointOrderIsConservedWhenConvertingCoordinates() {
+        val c1 = LatLng(1.0, 1.0)
+        val c2 = LatLng(2.0, 2.0)
+
+        val p1 = Utils.coordinateToPoint(c1)
+        val p2 = Utils.coordinateToPoint(c2)
+
+        val ps = Utils.coordinatesToStroke(listOf(c1, c2))
+
+        assertThat(ps.points, `is`(listOf(p1, p2)))
+    }
+}
