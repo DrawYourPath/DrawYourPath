c680c82f37e055b0de346d96907049097f9dcf17
Cristian Safta <saftakrist@gmail.com>
2023-05-13 21:43:45 +0200

    added some comments

--- app/src/androidTest/java/com/epfl/drawyourpath/mainpage/fragments/ChatFragmentTest.kt
+++ app/src/androidTest/java/com/epfl/drawyourpath/mainpage/fragments/ChatFragmentTest.kt
@@ -19,46 +19,57 @@ import org.junit.runner.RunWith
 @RunWith(AndroidJUnit4::class)
 class ChatFragmentTest {
 
+    // This rule provides functional testing of a single Activity
     @get:Rule
     val activityRule = ActivityScenarioRule(MainActivity::class.java)
 
+    // Test to verify if the chat fragment is displayed correctly
     @Test
     fun chatFragmentIsDisplayed() {
+        // Create mock database instance
         val database = MockDatabase()
+
+        // Launch the chat fragment in a container
         val scenario = launchFragmentInContainer(themeResId = R.style.Theme_Bootcamp) {
             ChatFragment(database)
         }
 
+        // Verify if the chat fragment is displayed
         onView(withId(R.id.chat_list_fragment)).check(matches(isDisplayed()))
     }
 
+    // Test to verify if the chat list is displayed in the chat fragment
     @Test
     fun chatListIsDisplayed() {
+        // Create mock database instance
         val database = MockDatabase()
+
+        // Launch the chat fragment in a container
         val scenario = launchFragmentInContainer(themeResId = R.style.Theme_Bootcamp) {
             ChatFragment(database)
         }
 
+        // Verify if the chat list recycler view is displayed
         onView(withId(R.id.chatListRecyclerView))
             .check(matches(isDisplayed()))
     }
 
+    // Test to verify if the chat list item is visible in the chat list recycler view
     @Test
     fun testChatListItemIsVisible() {
+        // Create mock database instance
         val database = MockDatabase()
+
+        // Launch the chat fragment in a container
         val scenario = launchFragmentInContainer(themeResId = R.style.Theme_Bootcamp) {
             ChatFragment(database)
         }
 
-        // Check if the first chat item in the list is visible
+        // Scroll to the first item in the chat list recycler view and verify if it's visible
         onView(withId(R.id.chatListRecyclerView))
             .perform(RecyclerViewActions.scrollToPosition<ChatAdapter.ChatViewHolder>(0))
             .check(matches(hasDescendant(withId(R.id.chatTitleTextView))))
     }
-
-
-
-
-
 }
+
 
--- app/src/androidTest/java/com/epfl/drawyourpath/mainpage/fragments/ChatOpenFragmentTest.kt
+++ app/src/androidTest/java/com/epfl/drawyourpath/mainpage/fragments/ChatOpenFragmentTest.kt
@@ -18,27 +18,35 @@ import org.junit.runner.RunWith
 @RunWith(AndroidJUnit4::class)
 class ChatOpenFragmentTest {
 
+    // Test to verify if the ChatOpenFragment is displayed correctly
     @Test
     fun chatOpenFragmentIsDisplayed() {
+        // Launch the ChatOpenFragment in a container
         val scenario = launchFragmentInContainer<ChatOpenFragment>(
             themeResId = R.style.Theme_Bootcamp
         )
 
+        // Verify if the ChatOpenFragment is displayed
         onView(withId(R.id.chat_open_fragment)).check(matches(isDisplayed()))
     }
 
+    // Test to verify if the messages RecyclerView is displayed in the ChatOpenFragment
     @Test
     fun messagesRecyclerViewIsDisplayed() {
+        // Launch the ChatOpenFragment in a container
         val scenario = launchFragmentInContainer<ChatOpenFragment>(
             themeResId = R.style.Theme_Bootcamp
         )
 
+        // Verify if the messages RecyclerView is displayed
         onView(withId(R.id.messagesRecyclerView))
             .check(matches(isDisplayed()))
     }
 
+    // Test to verify if typing a message updates the message EditText field
     @Test
     fun testTypingMessageUpdatesMessageEditText() {
+        // Launch the ChatOpenFragment in a container
         val scenario = launchFragmentInContainer<ChatOpenFragment>(
             themeResId = R.style.Theme_Bootcamp
         )
@@ -51,8 +59,10 @@ class ChatOpenFragmentTest {
         onView(withId(R.id.messageEditText)).check(matches(withText(message)))
     }
 
+    // Test to verify if sending a message adds it to the RecyclerView
     @Test
     fun testSendingMessageAddsMessageToRecyclerView() {
+        // Launch the ChatOpenFragment in a container
         val scenario = launchFragmentInContainer<ChatOpenFragment>(
             themeResId = R.style.Theme_Bootcamp
         )
@@ -69,16 +79,19 @@ class ChatOpenFragmentTest {
             .check(matches(hasDescendant(withText(message))))
     }
 
+    // Test to verify if the message EditText field is cleared after sending a message
     @Test
     fun messageEditTextIsClearedAfterSendingMessage() {
+        // Launch the ChatOpenFragment in a container
         val scenario = launchFragmentInContainer<ChatOpenFragment>(
             themeResId = R.style.Theme_Bootcamp
         )
 
+        // Type a message and send it
         onView(withId(R.id.messageEditText)).perform(typeText("Test message"), closeSoftKeyboard())
         onView(withId(R.id.sendMessageButton)).perform(click())
 
+        // Check if the messageEditText is cleared after sending the message
         onView(withId(R.id.messageEditText)).check(matches(withText("")))
     }
 }
-
