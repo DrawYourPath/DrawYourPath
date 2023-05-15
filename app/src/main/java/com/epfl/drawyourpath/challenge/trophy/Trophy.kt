9cc8f20669f44380f5867e4f4eeb172ea086ded4
Eric Kurmann <eric.kurmann@epfl.ch>
2023-05-15 20:22:09 +0200

    fix test

--- app/src/main/java/com/epfl/drawyourpath/challenge/trophy/Trophy.kt
+++ app/src/main/java/com/epfl/drawyourpath/challenge/trophy/Trophy.kt
@@ -16,9 +16,14 @@ data class Trophy(
 
     companion object {
         val sample = listOf(
-            Trophy("id1", "Star Shape", "draw a star with 5 branches", LocalDate.ofYearDay(2023, 92), 1),
+            Trophy(
+                "id1",
+                "Most  Longest  greatest  Tournament  ever  of  all  times ................ : )",
+                "This  is  a  long  description,  really  really  long...... still not long enough.... maybe now it is long enough",
+                LocalDate.ofYearDay(2023, 89),
+                1,
+            ),
             Trophy("id2", "ShapeSpear", "draw a spear on the map", LocalDate.ofYearDay(2023, 35), 2),
-            Trophy("id4", "Discover the earth", "draw the earth", LocalDate.ofYearDay(2023, 23), 3),
         )
     }
 }
