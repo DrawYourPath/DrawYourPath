b42f8956234c7e247e1f1ffe95d8bc2920c70630
Eric Kurmann <eric.kurmann@epfl.ch>
2023-05-18 19:48:13 +0200

    run auto format

--- format.bat
+++ format.bat
@@ -1,1 +1,10 @@
 java -jar ./programs/ktlint.jar --disabled_rules no-wildcard-imports -F ./app/**.kt
+if git diff-index --quiet HEAD --; then
+    echo "Code formatting matched"
+else
+    git config --global user.email "bot@drawyourpath.ch"
+    git config --global user.name "DrawYourPathBot"
+    git add .
+    git commit -m "action: auto-formatting"
+    git push
+    fi
