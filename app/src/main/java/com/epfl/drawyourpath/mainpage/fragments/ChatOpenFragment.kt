7a2954b92686fb0d37786c0ad8ba1c4b5616fc29
HugoHof <hugo.hof@epfl.ch>
2023-05-22 15:42:26 +0200

    reformat code

--- app/src/main/java/com/epfl/drawyourpath/mainpage/fragments/ChatOpenFragment.kt
+++ app/src/main/java/com/epfl/drawyourpath/mainpage/fragments/ChatOpenFragment.kt
@@ -6,7 +6,6 @@ import android.widget.EditText
 import android.widget.ImageButton
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
-import androidx.lifecycle.LiveData
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.epfl.drawyourpath.R
